27a58e87603360253ccee339c5e71241b292e01e
georgelgore <gore9213@gmail.com>
2017-10-16 16:08:24 -0400

    done”
    git commit -m done”

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,49 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+
+end
+
+def play(songs)
+
+  puts "Please enter a song or number:"
+    input = gets.chomp
+    songs.each_with_index do |song, index|
+      if input == song || input.to_i - 1 == index
+        return puts "Playing #{song}"
+      end
+    end
+    puts "Invalid input, please try again"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Please enter a command:"
+  input = gets.chomp
+  if input == "help"
+    help
+  elsif input == "list"
+    list(songs)
+  elsif input == "play"
+    play(songs)
+  elsif input == "exit"
+    exit_jukebox
+  end
+
+end
