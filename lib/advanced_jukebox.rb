4a69d834480777f649a63ce19cb08608d7ac6732
Yuliya Yasenetska <jasenetska@gmail.com>
2017-10-18 00:17:34 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,25 +2,28 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+ my_songs = {
+ "Go Go GO" => 'home/YuliyaYas/jukebox-cli/audio/Emerald-Park/01.mp3',
+ "LiberTeens" => '/home/YuliyaYas/jukebox-cli/audio/Emerald-Park/02.mp3',
+ "Hamburg" =>  '/home/YuliyaYas/jukebox-cli/audio/Emerald-Park/03.mp3',
+ "Guiding Light" => '/home/YuliyaYas/jukebox-cli/audio/Emerald-Park/04.mp3',
+ "Wolf" => '/home/YuliyaYas/jukebox-cli/audio/Emerald-Park/05.mp3',
+ "Blue" => '</home/YuliyaYas/jukebox-cli/audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => '/home/YuliyaYas/jukebox-cli/audio/Emerald-Park/07.mp3'
+ }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+ - help : displays this help message
+ - list : displays a list of songs you can play
+ - play : lets you choose a song to play
+ - exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
 
@@ -33,13 +36,24 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+   puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+  loop do
+    puts "Please enter a command: "
+    response = gets.chomp
+    case response
+      when "play" then play(songs)
+      when "list" then list(songs)
+      when "help" then help
+      when "exit" then exit_jukebox
+    end
+    break if response == "exit"
+  end
 end
